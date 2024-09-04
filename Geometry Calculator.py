d30fd044cb1015b6dd20c1847f716b9c7580ade8
Eric D. 9143 <111144791+XrxcGitHub@users.noreply.github.com>
2024-09-04 10:46:35 -0700

    Update Geometry Calculator.py

--- Geometry Calculator.py
+++ Geometry Calculator.py
@@ -7,34 +7,24 @@ continueProgram = True
 goodGuess = False
 
 # Function initializing
-def circle():
-    circleRadius = float(input("What is your radius: "))
+def circle(circleRadius):
     circleCircumference = math.pi * (2 * circleRadius)
     circleArea = math.pi * (circleRadius * circleRadius)
-    
-    print("Your radius is: " + str(circleRadius))
-    print("Your circumference is: " + str(circleCircumference))
-    print("Your area is: " + str(circleArea))
 
-def square():
-    squareWidth = float(input("What is your width: "))
+    return circleCircumference, circleArea
+
+def square(squareWidth):
     squarePerimeter = (2 * squareWidth) + (2 * squareWidth)
     squareArea = squareWidth * squareWidth
-    
-    print("Your width is: " + str(squareWidth))
-    print("Your perimeter is: " + str(squarePerimeter))
-    print("Your area is: " + str(squareArea))
 
-def rectangle():
-    rectangleWidth = float(input("What is your width: "))
-    rectangleLength = float(input("What is your length: "))
+    return squarePerimeter, squareArea
+
+def rectangle(rectangleWidth, rectangleLength):
     rectanglePerimeter = (2 * rectangleWidth) + (2 * rectangleLength)
     rectangleArea = rectangleWidth * rectangleLength
-    
-    print("Your width is: " + str(rectangleWidth))
-    print("Your length is: " + str(rectangleLength))
-    print("Your perimeter is: " + str(rectanglePerimeter))
-    print("Your area is: " + str(rectangleArea))
+
+    return rectanglePerimeter, rectangleArea
+
 
 
 # Run main program
@@ -45,29 +35,79 @@ while continueProgram == True:
     whatToLearn =  input("What do you want to learn about (circles, squares, or rectangles): ")
 
     if whatToLearn.lower() == "circles":
-        circle()
+        circleRadius = float(input("What is your radius: "))
+        circleResult = circle(circleRadius)
+
+        print("Your radius is: " + str(circleRadius))
+        print("Your circumference is: " + str(circleResult[0]))
+        print("Your area is: " + str(circleResult[1]))
+    
     elif whatToLearn.lower() == "squares":
-        square()
+        squareWidth = float(input("What is your width: "))
+        squareResult = square(squareWidth)
+
+        print("Your width is: " + str(squareWidth))
+        print("Your perimeter is: " + str(squareResult[0]))
+        print("Your area is: " + str(squareResult[1]))
+
     elif whatToLearn.lower() == "rectangles":
-        rectangle()
+        rectangleWidth = float(input("What is your width: "))
+        rectangleLength = float(input("What is your length: "))
+        rectangleResult = rectangle(rectangleWidth, rectangleLength)
+        
+        print("Your width is: " + str(rectangleWidth))
+        print("Your length is: " + str(rectangleLength))
+        print("Your perimeter is: " + str(rectangleResult[0]))
+        print("Your area is: " + str(rectangleResult[1]))
+    
     else:
+        
         while goodGuess == False:
+            
             newWhatToLearn = input("That input is not valid. What do you want to learn about (circles, squares, or rectangles): ")
+            
             if newWhatToLearn.lower() == "circles":
-                circle()
+                circleRadius = float(input("What is your radius: "))
+                circleResult = circle(circleRadius)
+
+                print("Your radius is: " + str(circleRadius))
+                print("Your circumference is: " + str(circleResult[0]))
+                print("Your area is: " + str(circleResult[1]))
+
                 goodGuess = True
+
             elif newWhatToLearn.lower() == "squares":
-                square()
+                squareWidth = float(input("What is your width: "))
+                squareResult = square(squareWidth)
+
+                print("Your width is: " + str(squareWidth))
+                print("Your perimeter is: " + str(squareResult[0]))
+                print("Your area is: " + str(squareResult[1]))
+
                 goodGuess = True
+
             elif newWhatToLearn.lower() == "rectangles":
-                rectangle()
+                rectangleWidth = float(input("What is your width: "))
+                rectangleLength = float(input("What is your length: "))
+                rectangleResult = rectangle(rectangleWidth, rectangleLength)
+                
+                print("Your width is: " + str(rectangleWidth))
+                print("Your length is: " + str(rectangleLength))
+                print("Your perimeter is: " + str(rectangleResult[0]))
+                print("Your area is: " + str(rectangleResult[1]))
+
                 goodGuess = True
+        
         goodGuess = False
     
     runAgain = input("Do you want to run the program again (yes or no): ")
+    
     if runAgain.lower() == "yes":
         continueProgram = True
+    
     elif runAgain.lower() == "no":
         continueProgram = False
+    
     else:
         print("Invalid response. Ending program.")
+        continueProgram = False
