565bc7a7fa343c6682d6b3e662693bf5c5c9872d
Eric D. 9143 <ericjamesdean1@gmail.com>
2025-04-30 19:12:00 -0700

    update fibonacci for ap csp

--- Old Fibonacci.py
+++ Old Fibonacci.py
@@ -2,27 +2,20 @@
 import os
 
 # Define the fibonacci function to find the corresponding value at a given position
-def fibonacci(position):
-    sequenceList = [1, 1]
-    posGood = True
-    if position == 1 or position == 2:
-        return 1
-    elif position <= 0:
+def fibonacci(n, side):
+    if n <= 0:
         return 0
+    elif n == 1 or n == 2:
+        return 1
     else:
-        while posGood == True:
-            if position != 2:
-                sequenceList.append(sequenceList[-1] + sequenceList[-2])
-                position -= 1
-            else:
-                posGood = False
-        return sequenceList[-1]
+        print("Running fibonacci with the", n, "position on the", side, "side.")
+        return (fibonacci(n-1, "L") + fibonacci(n-2, "R"))
 
 # Define the runFibonacci function for easy terminal clearing and program running
 def runFibonacci():
     os.system("clear")
     sequenceIndex = int(input("What position of the Fibonacci sequence would you like to find: "))
-    print(fibonacci(sequenceIndex))
+    print(fibonacci(sequenceIndex, "Start"))
 
 # Run main program
 runFibonacci()
